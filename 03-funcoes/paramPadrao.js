9baa175a6113d366fddfe19ce724622cf3ca29a1
gbxventura <gabriel.ventura20200@gmail.com>
2024-05-03 23:17:16 -0300

    Adicionando novas aulas

--- 03-funcoes/paramPadrao.js
+++ 03-funcoes/paramPadrao.js
@@ -32,32 +32,3 @@ console.log(soma3()); // 3
 console.log(soma3(3)); // 3
 console.log(soma3(1, 2, 3)); // 6
 console.log(soma3(0, 0, 0)); // 3
-
-const alunos = {
-  aluno1: {
-    nome: "Ana",
-    nota1: 2,
-    nota2: 10,
-    nota3: 5,
-  },
-  aluno2: {
-    nome: "Gabriel",
-    nota: 4,
-    nota2: 4,
-    nota3: 5,
-  },
-  aluno3: {
-    nome: "Roberto",
-    nota: 10,
-    nota2: 7,
-    nota3: 5,
-  },
-};
-
-const media = (n1 = 0, n2 = 0, n3 = 0) => {
-  return (n1 + n2 + n3) / 3;
-};
-
-console.log(`Aluno: ${alunos.aluno1.nome} |Nota: ${alunos.aluno1.nota}`);
-console.log(`Aluno: ${alunos.aluno2.nome} |Nota: ${alunos.aluno2.nota}`);
-console.log(`Aluno: ${alunos.aluno3.nome} |Nota: ${alunos.aluno3.nota}`);
